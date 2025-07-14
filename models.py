f5a69716750dc8e5d25a4a6392b228744291b060
Vibuda Siromin <74820803+VibudaSiromin@users.noreply.github.com>
2025-07-14 22:03:40 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 # ✅ models.py
 class Discriminator(nn.Module):
-    def __init__(self, hidden_size=4096):  # Match to your model hidden size
+    def __init__(self, hidden_size=4096): 
         super().__init__()
         self.dropout = nn.Dropout(0.3)
         self.classifier = nn.Linear(hidden_size, 2)
