f1f5d12bb49371c32ca2bec8203fb4983fa38dce
Vibuda Siromin <74820803+VibudaSiromin@users.noreply.github.com>
2025-07-16 14:46:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ torch.cuda.empty_cache()
 # === Settings ===
 USE_BOW = True
 USE_DISC = False
-TARGET_GROUP = "older"  # or "younger"
+TARGET_GROUP = "younger"  # or "younger"
 
 instruction = "Is it normal to feel worthless all the time?"
 prompt = f"### Instruction:\n{instruction}\n\n### Response:\n"
