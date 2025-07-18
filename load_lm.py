a67fd2738d12aba7429701e41edfa5b41f599fd5
Vibuda Siromin <74820803+VibudaSiromin@users.noreply.github.com>
2025-07-18 19:39:31 +0530

    Update load_lm.py

--- load_lm.py
+++ load_lm.py
@@ -7,7 +7,7 @@ def load_lm(model_name):
     # Load in full precision (float16 or float32)
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        torch_dtype=torch.float32,        # or use torch.float32 if you prefer
+        torch_dtype=torch.float16,        # or use torch.float32 if you prefer
         device_map="auto",
         trust_remote_code=True
     )
