bad35adbda7bd7bfc38516ed1887d490f04a8979
Vibuda Siromin <74820803+VibudaSiromin@users.noreply.github.com>
2025-07-23 19:59:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ USE_DISC = True
 TARGET_GROUP = "older"  # or  "younger"
 disc_target = 0 if TARGET_GROUP == "younger" else 1
 
-question = "I’m okay, just looking for some clarity and reflection."
+question = "I’ve been thinking a lot lately and wanted to share some thoughts."
 prompt = f"[INST] {question} [/INST]\nResponse:"
 
 print("USE_BOW", USE_BOW, "- USE_DISC",USE_DISC)
