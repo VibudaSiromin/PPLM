405a34947605da225625a5bfb8aabfca62ea9337
Vibuda Siromin <74820803+VibudaSiromin@users.noreply.github.com>
2025-07-23 17:37:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ gc.collect()
 torch.cuda.empty_cache()
 
 # === Settings ===
-USE_BOW = False
-USE_DISC = True
-TARGET_GROUP = "older"  # or  "younger"
+USE_BOW = True
+USE_DISC = False
+TARGET_GROUP = "younger"  # or  "older"
 disc_target = 0 if TARGET_GROUP == "younger" else 1
 
-question = "I had such a good conversation with an old friend today—feeling great!"
+question = "I finally got that job I’ve been dreaming about!"
 prompt = f"[INST] {question} [/INST]\nResponse:"
 
 print("USE_BOW", USE_BOW, "- USE_DISC",USE_DISC)
