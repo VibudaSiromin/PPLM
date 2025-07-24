21e3f4687940a246b144f8cdc33caf66d04085a3
Vibuda Siromin <74820803+VibudaSiromin@users.noreply.github.com>
2025-07-24 14:05:42 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -7,7 +7,7 @@ import torch
 from collections import Counter
 from nltk import ngrams
 
-fileName = 'Texts/Angry_younger_BoW.json'
+fileName = 'Texts/Angry_younger_Disc.json'
 
 # Load generated sentences from JSON file
 with open(fileName, 'r') as f:
