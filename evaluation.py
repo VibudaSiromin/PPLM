5a1eecb91b221e2a61aee9f0943d8a4086f79c63
Vibuda Siromin <74820803+VibudaSiromin@users.noreply.github.com>
2025-07-24 12:25:23 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -5,7 +5,7 @@ from rouge_score import rouge_scorer
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-fileName = 'Texts/Happy_older_BoW.json'
+fileName = 'Texts/Happy_older_Disc.json'
 
 # Load JSON file (replace 'generated_texts.json' with your file path)
 with open(fileName, 'r') as f:
