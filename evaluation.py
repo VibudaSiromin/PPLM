c055e8f74e3c8eadb468e8f746473b2cd659830b
Vibuda Siromin <74820803+VibudaSiromin@users.noreply.github.com>
2025-07-25 14:18:55 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -7,7 +7,7 @@ import torch
 from collections import Counter
 from nltk import ngrams
 
-fileName = 'Texts/Llama_Relax.json'
+fileName = 'Texts/Llama_Sad.json'
 
 # Load generated sentences from JSON file
 with open(fileName, 'r') as f:
